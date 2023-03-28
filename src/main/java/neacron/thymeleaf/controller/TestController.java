29feb7ebb11e32ea08d1043fb7f3d30a448d9fae
Denis Chernoluckiy <d.chernoluckiy@tg.dunice.net>
2023-03-28 09:12:24 +0300

    initial commit

---
+++ src/main/java/neacron/thymeleaf/controller/TestController.java
@@ -0,0 +1,9 @@
+package neacron.thymeleaf.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+
+}
---
+++ src/main/java/neacron/thymeleaf/dto/Item.java
@@ -0,0 +1,18 @@
+package neacron.thymeleaf;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@Data
+public class Item {
+
+    private String name;
+    private Date date;
+    private String description;
+    private String price;
+    private String image;
+}
